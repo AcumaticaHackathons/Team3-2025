637e57c93eab40b98cb07223d4fd40e6a8313f42
Patrick Chen <patrick711@gmail.com>
2025-01-25 19:49:39 -0800

    Invoice Graph

---
+++ Hackathon2025Team3/Graph/NTSOSOInvoiceEntryExt.cs
@@ -0,0 +1,48 @@
+using PX.Data;
+using PX.Data.BQL.Fluent;
+using PX.Objects.SO;
+using PX.SM;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Hackathon2025Team3.Graph
+{
+    public class NTSOInvoiceEntryExt : PXGraphExtension<SOInvoiceEntry>
+    {
+        public static bool IsActive() => true;
+
+
+
+        #region Views
+        public SelectFrom<NTNoteGroups>.Where<NTNoteGroups.noteid.IsEqual<SOInvoice.noteID.FromCurrent>>.View NTNotesGroups;
+
+        public SelectFrom<NTNote>.Where<NTNote.groupID.IsEqual<NTNoteGroups.groupID.FromCurrent>>.View NTNotes;
+        #endregion
+
+
+        #region event handlers
+        public virtual void _(Events.RowInserted<SOInvoice> e)
+        {
+            SOInvoice Row = (SOInvoice)e.Row;
+            if (Row == null)
+            {
+                return;
+            }
+
+            if (Row.NoteID != null)
+            {
+                if (NTNotesGroups.Select().Count == 0)
+                {
+                    NTNoteGroups NewNoteGroup = new NTNoteGroups();
+                    NewNoteGroup.GroupID = Guid.NewGuid();
+                    NewNoteGroup.Noteid = Row.NoteID;
+                    NTNotesGroups.Insert(NewNoteGroup);
+                }
+            }
+        }
+        #endregion
+    }
+}
