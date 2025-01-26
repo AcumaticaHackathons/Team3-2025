5f186462557815f5578f74eb437a10724137f047
Joseph Carroll <Joseph.Carroll@xbyte.com>
2025-01-25 21:09:50 -0600

    Add custom notes to SOOrderEntry

--- Hackathon2025Team3/Graph/NTSOOrderEntryExt.cs
+++ Hackathon2025Team3/Graph/NTSOOrderEntryExt.cs
@@ -5,6 +5,7 @@ using PX.SM;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 
 namespace Hackathon2025Team3
@@ -13,15 +14,51 @@ namespace Hackathon2025Team3
     {
         public static bool IsActive() => true;
 
-        public void Initialize() 
-        { 
-            
-        }
+        
 
         #region Views
-        public SelectFrom<NTNoteGroups>.Where<NTNoteGroups.NoteID.IsEqual<SOOrder.noteID.FromCurrent>>.View NTNotesGroups;
+        public SelectFrom<NTNoteGroups>.Where<NTNoteGroups.noteid.IsEqual<SOOrder.noteID.FromCurrent>>.View NTNotesGroups;
 
-        public SelectFrom<NTNote>.Where<NTNote.GroupID.IsEqual<NTNoteGroups.GroupID.FromCurrent>>.View NTNotes;
+        public SelectFrom<NTNote>.Where<NTNote.groupID.IsEqual<NTNoteGroups.groupID.FromCurrent>>.View NTNotes;
+        #endregion
+
+        #region Event Handlers
+        public virtual void _(Events.RowInserted<SOOrder> e)
+        {
+            SOOrder Row = (SOOrder)e.Row;
+            if (Row == null)
+            {
+                return;
+            }
+
+            if (Row.NoteID != null)
+            {
+                if (NTNotesGroups.Select().Count == 0)
+                {
+                    NTNoteGroups NewNoteGroup = new NTNoteGroups();
+                    NewNoteGroup.GroupID = Guid.NewGuid();
+                    NewNoteGroup.Noteid = Row.NoteID;
+                    NTNotesGroups.Insert(NewNoteGroup);
+                }
+            }
+        }
+
+        public virtual void _(Events.RowInserting<NTNote> e)
+        {
+            NTNote Row = (NTNote)e.Row;
+            if (Row == null)
+            {
+                return;
+            }
+
+            NTNoteGroups Group = this.NTNotesGroups.Select().FirstOrDefault();
+            if (Group == null)
+            {
+                return;
+            }
+
+            Row.GroupID = Group.GroupID;
+        }
         #endregion
 
         #region Actions
@@ -30,7 +67,10 @@ namespace Hackathon2025Team3
         [PXButton]
         public virtual IEnumerable addNewNote(PXAdapter adapter)
         {
-            this.NTNotes.AskExt();
+            if (this.NTNotes.AskExt() == WebDialogResult.OK) 
+            {
+                base.Base.Save.Press();
+            }
 
             return adapter.Get();
         }
---
+++ Hackathon2025Team3/Graph/NTSOShipmentEntryExt.cs
@@ -0,0 +1,14 @@
+using PX.Data;
+using PX.Objects.SO;
+using PX.SM;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Hackathon2025Team3.Graph
+{
+    public class NTSOShipmentEntryExt : PXGraphExtension<SOShipmentEntry>
+    {
+    }
+}
